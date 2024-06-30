68bbe0c011280c23c486c0b9cab62dd65ec2bfe4
Andelf <andelf@gmail.com>
2024-06-30 10:04:56 +0800

    feat(hpm6e): demo for hal and embassy

--- examples/hpm6e00evk/src/bin/blinky.rs
+++ examples/hpm6e00evk/src/bin/blinky.rs
@@ -2,63 +2,37 @@
 #![no_std]
 
 use embedded_hal::delay::DelayNs;
-use hpm_metapac::gpiom::vals;
+use hal::pac;
+use hpm_hal::gpio::{Level, Output};
 use riscv::delay::McycleDelay;
-use {defmt_rtt as _, hpm_metapac as pac, panic_halt as _, riscv_rt as _};
+use {defmt_rtt as _, hpm_hal as hal, panic_halt as _, riscv_rt as _};
 
-#[riscv_rt::entry]
+#[hal::entry]
 fn main() -> ! {
+    let p = hal::init(Default::default());
     // default clock
-    let mut delay = McycleDelay::new(600_000_000);
+    let mut delay = McycleDelay::new(hal::sysctl::clocks().cpu0.0);
 
-    // ugly but works
-    pac::SYSCTL.group0(0).set().modify(|w| w.0 = 0xFFFFFFFF);
-    pac::SYSCTL.group0(1).set().modify(|w| w.0 = 0xFFFFFFFF);
-    pac::SYSCTL.group0(2).set().modify(|w| w.0 = 0xFFFFFFFF);
-    pac::SYSCTL.group0(3).set().modify(|w| w.0 = 0xFFFFFFFF);
+    // all leds are active low
 
-    pac::SYSCTL.affiliate(0).set().write(|w| w.set_link(1));
-
-    /*
-    pac::IOC.pad(142).func_ctl().modify(|w| w.set_alt_select(0));
-    pac::IOC.pad(142).pad_ctl().write(|w| {
-        w.set_pe(true);
-    });
-    */
-
-    const PE: usize = 4;
-    pac::GPIOM.assign(PE).pin(14).modify(|w| {
-        w.set_select(vals::PinSelect::CPU0_FGPIO); // FGPIO0
-        w.set_hide(0b01); // invisible to GPIO0
-    });
-    pac::GPIOM.assign(PE).pin(15).modify(|w| {
-        w.set_select(vals::PinSelect::CPU0_FGPIO); // FGPIO0
-        w.set_hide(0b01); // invisible to GPIO0
-    });
-    pac::GPIOM.assign(PE).pin(4).modify(|w| {
-        w.set_select(vals::PinSelect::CPU0_FGPIO); // FGPIO0
-        w.set_hide(0b01); // invisible to GPIO0
-    });
-
-    pac::FGPIO
-        .oe(PE)
-        .set()
-        .write(|w| w.set_direction((1 << 14) | (1 << 15) | (1 << 4)));
+    let mut r = Output::new(p.PE14, Level::Low, Default::default());
+    let mut g = Output::new(p.PE15, Level::Low, Default::default());
+    let mut b = Output::new(p.PE04, Level::Low, Default::default());
 
     defmt::info!("Board init!");
 
     loop {
         defmt::info!("tick");
 
-        pac::FGPIO.do_(PE).toggle().write(|w| w.set_output(1 << 14));
+        r.toggle();
 
         delay.delay_ms(100);
 
-        pac::FGPIO.do_(PE).toggle().write(|w| w.set_output(1 << 15));
+        g.toggle();
 
         delay.delay_ms(100);
 
-        pac::FGPIO.do_(PE).toggle().write(|w| w.set_output(1 << 4));
+        b.toggle();
 
         delay.delay_ms(100);
     }
---
+++ examples/hpm6e00evk/src/bin/embassy_blinky.rs
@@ -0,0 +1,52 @@
+#![no_main]
+#![no_std]
+#![feature(type_alias_impl_trait)]
+
+use embassy_executor::Spawner;
+use embassy_time::Timer;
+use hal::gpio::Pin as _;
+use hpm_hal::gpio::{AnyPin, Level, Output};
+use {defmt_rtt as _, hpm_hal as hal, riscv_rt as _};
+
+#[embassy_executor::task(pool_size = 3)]
+async fn blink(pin: AnyPin, interval_ms: u32) {
+    // all leds are active low
+    let mut led = Output::new(pin, Level::Low, Default::default());
+
+    loop {
+        led.toggle();
+
+        Timer::after_millis(interval_ms as u64).await;
+    }
+}
+
+#[embassy_executor::main(entry = "hpm_hal::entry")]
+async fn main(spawner: Spawner) -> ! {
+    let p = hal::init(Default::default());
+
+    defmt::info!("Board init!");
+
+    spawner.spawn(blink(p.PE14.degrade(), 100)).unwrap();
+    spawner.spawn(blink(p.PE15.degrade(), 200)).unwrap();
+    spawner.spawn(blink(p.PE04.degrade(), 300)).unwrap();
+
+    defmt::info!("Tasks init!");
+
+    loop {
+        defmt::info!("tick");
+
+        Timer::after_millis(1000).await;
+    }
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    let mut err = heapless::String::<1024>::new();
+
+    use core::fmt::Write as _;
+
+    write!(err, "panic: {}", _info).ok();
+
+    defmt::info!("{}", err.as_str());
+    loop {}
+}
