54986d2d5e6ec88458c5fe47e4806617b6956179
Andelf <andelf@gmail.com>
2024-07-02 10:04:42 +0800

    enhance: refine openocd scripts

--- run-openocd.sh
+++ run-openocd.sh
@@ -7,17 +7,15 @@ export HPM_SDK_BASE=`pwd`/../hpm_sdk
 
 # openocd -f $OPENOCD_CFG/probes/ft2232.cfg -f $OPENOCD_CFG/soc/hpm5300.cfg -f $OPENOCD_CFG/boards/hpm5300evk.cfg
 
-export FAMILY=hpm6e00
-export BOARD=hpm6e00evk
-export PROBE=ft2232
+export FAMILY=hpm6e00; export BOARD=hpm6e00evk
 
-# export BOARD=hpm5301evklite
-# export BOARD=hpm5300evk
-# export PROBE=cmsis_dap
+# export FAMILY=hpm5300; export BOARD=hpm5301evklite
+# export FAMILY=hpm5300; export BOARD=hpm5300evk
+
+# export FAMILY=hpm6300; export BOARD=hpm6300evk
+
+
 export PROBE=ft2232
 # export PROBE=cmsis_dap
-
-#export FAMILY=hpm5300
-export FAMILY=hpm6e00
 
 openocd -c "set HPM_SDK_BASE ${HPM_SDK_BASE}; set BOARD ${BOARD}; set PROBE ${PROBE};" -f ${HPM_SDK_BASE}/boards/openocd/${FAMILY}_all_in_one.cfg
