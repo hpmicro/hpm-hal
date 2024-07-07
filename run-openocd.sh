8d88d0975300a7d703c64d66a213b96bfde6d70e
Andelf <andelf@gmail.com>
2024-07-08 03:25:56 +0800

    dev: refine openocd start script

--- run-openocd.sh
+++ run-openocd.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-# export HPM_SDK_BASE=/path/to/hpm_sdk
+_PWD=$(realpath $(dirname $0))
 
-export HPM_SDK_BASE=`pwd`/../hpm_sdk
+# if path to HPM_SDK_BASE is not set, then set it to default value
+if [ -z "$HPM_SDK_BASE" ]; then
+    export HPM_SDK_BASE=$(realpath "${_PWD}/../hpm_sdk")
+fi
+
+if [ ! -d "$HPM_SDK_BASE" ]; then
+    echo "HPM_SDK_BASE is not set correctly. Please set it to the correct path."
+    exit 1
+fi
+
+echo "Using HPM_SDK_BASE=${HPM_SDK_BASE}"
 
 # openocd -f $OPENOCD_CFG/probes/ft2232.cfg -f $OPENOCD_CFG/soc/hpm5300.cfg -f $OPENOCD_CFG/boards/hpm5300evk.cfg
 
-export FAMILY=hpm6e00; export BOARD=hpm6e00evk
+# export FAMILY=hpm6e00; export BOARD=hpm6e00evk
 
 # export FAMILY=hpm5300; export BOARD=hpm5301evklite
-# export FAMILY=hpm5300; export BOARD=hpm5300evk
+export FAMILY=hpm5300; export BOARD=hpm5300evk
 
 # export FAMILY=hpm6300; export BOARD=hpm6300evk
 
