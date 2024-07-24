d867fcecb60084eca262f850eb794325836ac5ac
Haobo Gu <haobogu@outlook.com>
2024-07-24 19:45:28 +0800

    feat(example): add usb example
    Signed-off-by: Haobo Gu <haobogu@outlook.com>

--- src/usb/control_pipe.rs
+++ src/usb/control_pipe.rs
@@ -20,6 +20,7 @@ impl<'d, T: Instance> embassy_usb_driver::ControlPipe for ControlPipe<'d, T> {
     }
 
     async fn setup(&mut self) -> [u8; 8] {
+        defmt::info!("ControlPipe::setup");
         unsafe {
             init_qhd(&EpConfig {
                 // Must be EndpointType::Control
@@ -50,6 +51,7 @@ impl<'d, T: Instance> embassy_usb_driver::ControlPipe for ControlPipe<'d, T> {
         _first: bool,
         _last: bool,
     ) -> Result<usize, embassy_usb_driver::EndpointError> {
+        defmt::info!("ControlPipe::dataout");
         self.ep_out.transfer(buf).map_err(|_e| EndpointError::BufferOverflow)?;
         Ok(buf.len())
     }
@@ -60,6 +62,7 @@ impl<'d, T: Instance> embassy_usb_driver::ControlPipe for ControlPipe<'d, T> {
         _first: bool,
         _last: bool,
     ) -> Result<(), embassy_usb_driver::EndpointError> {
+        defmt::info!("ControlPipe::datain");
         self.ep_in.transfer(data).map_err(|_e| EndpointError::BufferOverflow)?;
         Ok(())
     }
@@ -67,15 +70,19 @@ impl<'d, T: Instance> embassy_usb_driver::ControlPipe for ControlPipe<'d, T> {
     /// Accept a control request.
     ///
     /// Causes the STATUS packet for the current request to be ACKed.
-    async fn accept(&mut self) {}
+    async fn accept(&mut self) {
+        defmt::info!("ControlPipe::accept");
+    }
 
     async fn reject(&mut self) {
+        defmt::info!("ControlPipe::reject");
         // Reject, set IN+OUT to stall
         self.ep_in.set_stall();
         self.ep_out.set_stall();
     }
 
     async fn accept_set_address(&mut self, addr: u8) {
+        defmt::info!("ControlPipe::accept_set_address");
         // Response with STATUS?
         // self.ep_in.transfer(&[]);
 
