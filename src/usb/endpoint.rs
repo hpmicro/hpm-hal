89499c241d626aaceb6e91f225e72d4785293cb6
HaoboGu <haobogu@outlook.com>
2024-08-05 23:48:07 +0800

    feat(usb): make usb enumerable
    Signed-off-by: HaoboGu <haobogu@outlook.com>

--- src/usb/endpoint.rs
+++ src/usb/endpoint.rs
@@ -18,21 +18,20 @@ pub struct EpConfig {
 pub struct Endpoint<'d, T: Instance> {
     pub(crate) _phantom: PhantomData<&'d mut T>,
     pub(crate) info: EndpointInfo,
-    pub(crate) buffer: [u8; 64],
 }
 
 impl<'d, T: Instance> Endpoint<'d, T> {
     pub(crate) fn start_transfer(&mut self) {
         let ep_num = self.info.addr.index();
-        let ep_idx = 2 * ep_num + self.info.addr.is_in() as usize;
-        let offset = if ep_idx % 2 == 1 { ep_idx / 2 + 16 } else { ep_idx / 2 };
+        // let ep_idx = 2 * ep_num + self.info.addr.is_in() as usize;
+        // let offset = if ep_idx % 2 == 1 { ep_idx / 2 + 16 } else { ep_idx / 2 };
 
-        defmt::info!(
-            "Start transfer on endpoint {}, dir_in?: {}, offset: {}",
-            ep_num,
-            self.info.addr.is_in(),
-            offset
-        );
+        // defmt::info!(
+        //     "Start transfer on endpoint {}, dir_in?: {}, offset: {}",
+        //     ep_num,
+        //     self.info.addr.is_in(),
+        //     offset
+        // );
 
         let r = T::info().regs;
         r.endptprime().modify(|w| {
@@ -126,31 +125,31 @@ impl<'d, T: Instance> Endpoint<'d, T> {
 
         // Link qtd to qhd
         let first_idx = first_qtd.unwrap();
-        unsafe {
-            defmt::info!(
-                "Check first qtd idx: {}, addr: {:x} and content:
-                next_dtd_word: 0x{:x}
-                total_bytes: {}, ioc: {}, c_page: {}, multO: {}, status: 0b{:b}
-                Buffer0 + offset: {:x}
-                Buffer1 : {:x}
-                Buffer2 : {:x}
-                Buffer3 : {:x}
-                Buffer4 : {:x}",
-                first_idx,
-                DCD_DATA.qtd_list.qtd(first_idx).as_ptr() as u32,
-                DCD_DATA.qtd_list.qtd(first_idx).next_dtd().read().0,
-                DCD_DATA.qtd_list.qtd(first_idx).qtd_token().read().total_bytes(),
-                DCD_DATA.qtd_list.qtd(first_idx).qtd_token().read().ioc(),
-                DCD_DATA.qtd_list.qtd(first_idx).qtd_token().read().c_page(),
-                DCD_DATA.qtd_list.qtd(first_idx).qtd_token().read().multo(),
-                DCD_DATA.qtd_list.qtd(first_idx).qtd_token().read().status(),
-                DCD_DATA.qtd_list.qtd(first_idx).buffer(0).read().0,
-                DCD_DATA.qtd_list.qtd(first_idx).buffer(1).read().0,
-                DCD_DATA.qtd_list.qtd(first_idx).buffer(2).read().0,
-                DCD_DATA.qtd_list.qtd(first_idx).buffer(3).read().0,
-                DCD_DATA.qtd_list.qtd(first_idx).buffer(4).read().0,
-            );
-        }
+        // unsafe {
+        //     defmt::info!(
+        //         "Check first qtd idx: {}, addr: {:x} and content:
+        //         next_dtd_word: 0x{:x}
+        //         total_bytes: {}, ioc: {}, c_page: {}, multO: {}, status: 0b{:b}
+        //         Buffer0 + offset: {:x}
+        //         Buffer1 : {:x}
+        //         Buffer2 : {:x}
+        //         Buffer3 : {:x}
+        //         Buffer4 : {:x}",
+        //         first_idx,
+        //         DCD_DATA.qtd_list.qtd(first_idx).as_ptr() as u32,
+        //         DCD_DATA.qtd_list.qtd(first_idx).next_dtd().read().0,
+        //         DCD_DATA.qtd_list.qtd(first_idx).qtd_token().read().total_bytes(),
+        //         DCD_DATA.qtd_list.qtd(first_idx).qtd_token().read().ioc(),
+        //         DCD_DATA.qtd_list.qtd(first_idx).qtd_token().read().c_page(),
+        //         DCD_DATA.qtd_list.qtd(first_idx).qtd_token().read().multo(),
+        //         DCD_DATA.qtd_list.qtd(first_idx).qtd_token().read().status(),
+        //         DCD_DATA.qtd_list.qtd(first_idx).buffer(0).read().0,
+        //         DCD_DATA.qtd_list.qtd(first_idx).buffer(1).read().0,
+        //         DCD_DATA.qtd_list.qtd(first_idx).buffer(2).read().0,
+        //         DCD_DATA.qtd_list.qtd(first_idx).buffer(3).read().0,
+        //         DCD_DATA.qtd_list.qtd(first_idx).buffer(4).read().0,
+        //     );
+        // }
 
         unsafe {
             if ep_num == 0 {
@@ -163,29 +162,29 @@ impl<'d, T: Instance> Endpoint<'d, T> {
                 // T **MUST** be set to 0
                 w.set_t(false);
             });
-            let qhd: crate::usb::types_v53::Qhd = DCD_DATA.qhd_list.qhd(ep_idx);
+            // let qhd: crate::usb::types_v53::Qhd = DCD_DATA.qhd_list.qhd(ep_idx);
 
-            defmt::info!(
-                "ENDPTLISTADDR: {:x}
-                Check qhd after setting: qhd_idx: {}
-                1st word: mult: {}, zlt: {}, mps: {}, ios: {}
-                2nd word: cur dtd: {:x}
-                3rd word: next_dtd + t: {:x}
-                total_bytes: {}, ioc: {}, c_page: {}, multO: {}, status: 0b{:b}",
-                T::info().regs.endptlistaddr().read().0,
-                ep_idx,
-                qhd.cap().read().iso_mult(),
-                qhd.cap().read().zero_length_termination(),
-                qhd.cap().read().max_packet_size(),
-                qhd.cap().read().ios(),
-                qhd.cur_dtd().read().0, // 2nd word
-                qhd.next_dtd().read().0,
-                qhd.qtd_token().read().total_bytes(),
-                qhd.qtd_token().read().ioc(),
-                qhd.qtd_token().read().c_page(),
-                qhd.qtd_token().read().multo(),
-                qhd.qtd_token().read().status(),
-            );
+            // defmt::info!(
+            //     "ENDPTLISTADDR: {:x}
+            //     Check qhd after setting: qhd_idx: {}
+            //     1st word: mult: {}, zlt: {}, mps: {}, ios: {}
+            //     2nd word: cur dtd: {:x}
+            //     3rd word: next_dtd + t: {:x}
+            //     total_bytes: {}, ioc: {}, c_page: {}, multO: {}, status: 0b{:b}",
+            //     T::info().regs.endptlistaddr().read().0,
+            //     ep_idx,
+            //     qhd.cap().read().iso_mult(),
+            //     qhd.cap().read().zero_length_termination(),
+            //     qhd.cap().read().max_packet_size(),
+            //     qhd.cap().read().ios(),
+            //     qhd.cur_dtd().read().0, // 2nd word
+            //     qhd.next_dtd().read().0,
+            //     qhd.qtd_token().read().total_bytes(),
+            //     qhd.qtd_token().read().ioc(),
+            //     qhd.qtd_token().read().c_page(),
+            //     qhd.qtd_token().read().multo(),
+            //     qhd.qtd_token().read().status(),
+            // );
         }
 
         // Start transfer
@@ -243,6 +242,19 @@ impl<'d, T: Instance> embassy_usb_driver::Endpoint for Endpoint<'d, T> {
 
 impl<'d, T: Instance> EndpointOut for Endpoint<'d, T> {
     async fn read(&mut self, buf: &mut [u8]) -> Result<usize, embassy_usb_driver::EndpointError> {
+        defmt::info!("EndpointOut::read: data: {=[u8]}", buf);
+        let ep_num = self.info.addr.index();
+        let r = T::info().regs;
+        poll_fn(|cx| {
+            EP_OUT_WAKERS[ep_num].register(cx.waker());
+
+            if r.endptcomplete().read().0 & (1 << ep_num) != 0 {
+                Poll::Ready(())
+            } else {
+                Poll::Pending
+            }
+        })
+        .await;
         self.transfer(buf).unwrap();
         Ok(buf.len())
     }
@@ -250,6 +262,20 @@ impl<'d, T: Instance> EndpointOut for Endpoint<'d, T> {
 
 impl<'d, T: Instance> EndpointIn for Endpoint<'d, T> {
     async fn write(&mut self, buf: &[u8]) -> Result<(), embassy_usb_driver::EndpointError> {
+        defmt::info!("EndpointIn::write: data: {=[u8]}", buf);
+        let ep_num = self.info.addr.index();
+        let offset = ep_num + self.info.addr.is_in() as usize * 16;
+        let r = T::info().regs;
+        poll_fn(|cx| {
+            EP_IN_WAKERS[ep_num].register(cx.waker());
+            // It's IN endpoint, so check the bit 16 + offset
+            if r.endptcomplete().read().0 & (1 << (offset + 16)) != 0 {
+                Poll::Ready(())
+            } else {
+                Poll::Pending
+            }
+        })
+        .await;
         self.transfer(buf).unwrap();
         Ok(())
     }
