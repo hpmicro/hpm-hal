a6706e778a3904eceec00bc729784d39ae136494
Andelf <andelf@gmail.com>
2024-08-08 00:42:53 +0800

    chore(scripts): add jlink to probe scripts

--- run-openocd.sh
+++ run-openocd.sh
@@ -29,5 +29,6 @@ export FAMILY=hpm5300; export BOARD=hpm5300evk
 
 export PROBE=ft2232
 # export PROBE=cmsis_dap
+# export PROBE=jlink
 
 openocd -c "set HPM_SDK_BASE ${HPM_SDK_BASE}; set BOARD ${BOARD}; set PROBE ${PROBE};" -f ${HPM_SDK_BASE}/boards/openocd/${FAMILY}_all_in_one.cfg
