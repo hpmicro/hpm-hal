2842bec1c3e5473415538f22c1ae4d7a5caf5b97
Haobo Gu <haobogu@outlook.com>
2024-09-19 17:25:52 +0800

    fix(hpm53): remove ADC1/DAC for hpm5301
    Signed-off-by: Haobo Gu <haobogu@outlook.com>

--- src/patches/hpm53.rs
+++ src/patches/hpm53.rs
@@ -3,8 +3,11 @@ use crate::pac::sysctl::vals::AnaClkMux;
 use crate::pac::SYSCTL;
 
 impl_ana_clock_periph!(ADC0, ANA0, ADC0, adcclk, 0);
+#[cfg(not(feature = "hpm5301"))]
 impl_ana_clock_periph!(ADC1, ANA1, ADC1, adcclk, 1);
+#[cfg(not(feature = "hpm5301"))]
 impl_ana_clock_periph!(DAC0, ANA2, DAC0, dacclk, 0);
+#[cfg(not(feature = "hpm5301"))]
 impl_ana_clock_periph!(DAC1, ANA3, DAC1, dacclk, 1);
 
 #[cfg(peri_qei0)]
