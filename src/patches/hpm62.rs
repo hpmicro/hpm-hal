1b60876f89f495c900aebe195e984ee73687514e
Andelf <andelf@gmail.com>
2024-10-11 13:32:42 +0800

    fix(hpm6200): refine clock patches

--- src/patches/hpm62.rs
+++ src/patches/hpm62.rs
@@ -5,5 +5,40 @@ use crate::pac::SYSCTL;
 impl_ana_clock_periph!(ADC0, ANA0, ADC0, adcclk, 0);
 impl_ana_clock_periph!(ADC1, ANA1, ADC1, adcclk, 1);
 impl_ana_clock_periph!(ADC2, ANA2, ADC2, adcclk, 2);
-impl_ana_clock_periph!(DAC0, ANA3, DAC0, dacclk, 0);
-impl_ana_clock_periph!(DAC1, ANA4, DAC1, dacclk, 1);
+
+#[cfg(dac)]
+mod _adc_clock_patch {
+    use super::*;
+
+    impl_ana_clock_periph!(DAC0, ANA3, DAC0, dacclk, 0);
+    impl_ana_clock_periph!(DAC1, ANA4, DAC1, dacclk, 1);
+}
+
+#[cfg(qei)]
+mod _qei_clock_patch {
+    use super::*;
+
+    impl crate::sysctl::SealedClockPeripheral for peripherals::QEI0 {
+        const SYSCTL_CLOCK: usize = usize::MAX; // AHB
+        const SYSCTL_RESOURCE: usize = crate::pac::resources::MOT0;
+    }
+    impl crate::sysctl::ClockPeripheral for peripherals::QEI0 {}
+
+    impl crate::sysctl::SealedClockPeripheral for peripherals::QEI1 {
+        const SYSCTL_CLOCK: usize = usize::MAX; // AHB
+        const SYSCTL_RESOURCE: usize = crate::pac::resources::MOT1;
+    }
+    impl crate::sysctl::ClockPeripheral for peripherals::QEI1 {}
+
+    impl crate::sysctl::SealedClockPeripheral for peripherals::QEI2 {
+        const SYSCTL_CLOCK: usize = usize::MAX; // AHB
+        const SYSCTL_RESOURCE: usize = crate::pac::resources::MOT2;
+    }
+    impl crate::sysctl::ClockPeripheral for peripherals::QEI2 {}
+
+    impl crate::sysctl::SealedClockPeripheral for peripherals::QEI3 {
+        const SYSCTL_CLOCK: usize = usize::MAX; // AHB
+        const SYSCTL_RESOURCE: usize = crate::pac::resources::MOT3;
+    }
+    impl crate::sysctl::ClockPeripheral for peripherals::QEI3 {}
+}
